c3f631dd57147717bcc1f59f923fbe89f2fdea9b
Werter Pires <werterpires23@hotmail.com>
2025-09-06 21:35:04 -0300

    add organizations-members

---
+++ src/app/organization-members/organization-members.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-members',
+  imports: [],
+  templateUrl: './organization-members.component.html',
+  styleUrl: './organization-members.component.css'
+})
+export class OrganizationMembersComponent {
+
+}
