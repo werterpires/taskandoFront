19c14363b097506b450dca1f0ebc2454ce5703e8
Werter Pires <werterpires23@hotmail.com>
2025-11-13 19:27:24 -0300

    refactor: remove organization and member management components and their associated files
    - Deleted `SeeOrganizationMembersComponent` and its HTML, CSS, and spec files.
    - Removed `CreateOrganizationComponent` and its HTML, CSS, and spec files.
    - Eliminated `OrganizationsComponent` along with its HTML, CSS, and spec files.
    - Removed `SeeOrganizationComponent` and its HTML, CSS, and spec files.
    - Deleted related service files and types for organizations and members.
    - Cleaned up unused styles and templates across the organization management module.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,9 @@
 import { Routes } from '@angular/router';
 import { LogonComponent } from './logon/logon.component';
 import { LoginComponent } from './login/login.component';
-import { OrganizationsComponent } from './organizations/organizations.component';
-import { DepartmentsComponent } from './departments/departments.component';
+
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: OrganizationsComponent,
-  },
   {
     path: 'logon',
     component: LogonComponent,
@@ -16,13 +11,5 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
-  },
-  {
-    path: 'organizations',
-    component: OrganizationsComponent,
-  },
-  {
-    path: 'departments',
-    component: DepartmentsComponent,
   },
 ];
